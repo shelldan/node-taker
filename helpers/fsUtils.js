60588990fd730ae2fb43b88c5003f2710dd49c81
shelldan <shelldanlee@gmail.com>
2022-10-19 12:43:30 -0500

    update writeFile

--- helpers/fsUtils.js
+++ helpers/fsUtils.js
@@ -2,19 +2,25 @@
 const fs = require('fs');
 const util = require('util');
 
+const noteData = '../db/db.json';
+
 // Promise version of fs.readFile
 const readFromFile = util.promisify(fs.readFile);
+
 /**
  *  Function to write data to the JSON file given a destination and some content
  *  @param {string} destination The file you want to write to.
  *  @param {object} content The content you want to write to the file.
  *  @returns {void} Nothing
  */
+
+//question: why use writeFile to delete the note? because we are going to write a new file without the delete note (by filter out the delete note id)
 const writeToFile = (destination, content) =>
   //fs.writeFile() method replaces the specified file and content if it exists. If the file does not exists, a new file, containing the specified content, will be created
   fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
     err ? console.error(err) : console.info(`\nData written to ${destination}`)
   );
+
 /**
  *  Function to read data from a given a file and append some content
  *  @param {object} content The content you want to append to the file.
